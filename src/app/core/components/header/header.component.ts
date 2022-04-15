a7fa73bd82b8283ff9d4f3c5716fa0038f63cc47
KaloyanMarinov <kaloyanrumenov@gmail.com>
2022-04-15 10:10:14 +0300

    Update NavBar

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
-import { getIsAuthenticated } from '../../+store/auth/auth-selectors';
+import { getAuthUser, getIsAuthenticated } from '../../+store/auth/auth-selectors';
+import { IUser } from '../../auth/interfaces';
 
 @Component({
   selector: 'app-header',
@@ -10,10 +11,12 @@ import { getIsAuthenticated } from '../../+store/auth/auth-selectors';
 })
 export class HeaderComponent implements OnInit {
   isLogged$!: Observable<boolean>;
+  user$!: Observable<IUser>;
 
   constructor(private store: Store) { }
 
   ngOnInit(): void {
     this.isLogged$ = this.store.select(getIsAuthenticated);
+    this.user$ = this.store.select(getAuthUser);
   }
 }
