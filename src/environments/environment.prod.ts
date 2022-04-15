222d9cbf5305e6b749ffcd9d86c0f776a396ca8e
KaloyanMarinov <kaloyanrumenov@gmail.com>
2022-04-15 18:55:58 +0300

    Deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  baseUrl: 'https://baas.kinvey.com',
+  appKey: 'kid_B1T8Ub5Xc',
+  appSecret: '921a96945cfb41a9b80849af1ce74ffa',
+  masterSecret: 'ff173dbfa041410ba300439f1db699a8',
 };
