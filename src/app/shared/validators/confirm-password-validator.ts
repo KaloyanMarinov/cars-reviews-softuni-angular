b858607692334d1ba26f392faab93dc0a2077b77
KaloyanMarinov <kaloyanrumenov@gmail.com>
2022-04-16 11:03:59 +0300

    Fix Confirm Password

--- src/app/shared/validators/confirm-password-validator.ts
+++ src/app/shared/validators/confirm-password-validator.ts
@@ -2,7 +2,7 @@ import { AbstractControl, ValidatorFn } from "@angular/forms";
 
 export function confirmPasswords(passwordFormControl: AbstractControl) {
   const validtorFn: ValidatorFn = (confirmPasswordFormControl: AbstractControl) => {
-    if (passwordFormControl.value !== confirmPasswordFormControl.value) {
+    if (passwordFormControl.value !== confirmPasswordFormControl.value || !confirmPasswordFormControl.value) {
       return {
         confirmPasswords: true
       }
